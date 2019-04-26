d828031880045345658636819503220a1bedcb55
Florian Wallner <florian.wallner01@gmail.com>
2019-04-26 02:37:02 +0200

    das

--- Lua_Test_Directory/init.lua
+++ Lua_Test_Directory/init.lua
@@ -9,37 +9,6 @@ print("Test! System: " .. _LUAENV.ENVSYS)
 
 local PORT_A = "ttyS0"
 
-local data = {}
-
-data[1] = 0x83
-data[2] = 0x00
-data[3] = 0xFF
-data[4] = 0x53
-data[5] = 0x00
-data[6] = 0xFF
-data[7] = 0x85
-data[8] = 0x53
-
-for k,v in pairs(data) do
-	data[k] = string.char(v)
-end
-
-
-
-local buff = table.concat(data)
-
---net.dumpUDP("10.0.0.8", 64394, buff)
-
--- local s = net.connect("10.0.0.8", 62770)
--- print(s)
-
--- net.openUDP(1666, false, function(data, ip)
-	-- print("call udp!")
-	-- print("IP: " .. ip)
-	-- print("Data: " .. data)
--- end)
-
-
 local s = 0
 s = net.openTCP(666, 5, function()
 	print("call tcp!")
