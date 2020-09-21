35ccf0247bd8f8701ade7d3712014774e03b151d
Florian Wallner <florian.wallner01@gmail.com>
2020-09-22 01:28:54 +0200

    Update init.lua

--- Lua_Test_Directory/init.lua
+++ Lua_Test_Directory/init.lua
@@ -15,13 +15,16 @@ end
 print("\n\n\nInitializing IntelliCon Host\n")
 
 udp.open(1337, false, function(data, ip)
-	print(data[1])
+	print(data)
 	if data[0] == '$' then -- this is a broadcast
 		print("Received Broadcast from :" .. ip .. "\n")
 		
 	else 
 	
 	end
-
 end)
 
+think.add("tteses", 1000, function()
+	print("boop")
+	udp.dump("10.0.0.10", 13337, "aaaa")
+end)
